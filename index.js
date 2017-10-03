e4be8d32a2ecd28edeb379b6ab1e22495a9b1994
ProjectManager <projectmanager.mck@gmail.com>
2017-10-03 11:21:30 -0500

    primera ventana

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict'//usar el modo estricto de javascript para avitar problemas en el futuro
 
 //El objeto app es el que controla el ciclo de vida de la aplicacion, como el reseteo, el detenerla
-const { app } = require ('electron')
-
 //El objeto BrowserWindow es el que nos permite cargar todo el contenido visual de la aplicación de escritorio.
-
-console.dir(app)
+const { app, BrowserWindow } = require ('electron')
 
 //Esuchamos un evento basico que se ejecuta antes de que el aplicativo se ejecuta
 app.on('before-quit', () => {
@@ -13,4 +10,14 @@ app.on('before-quit', () => {
 
 })
 
-app.quit()
+app.on('ready', () => {
+	//Creamos una ventana basica
+	let win = new BrowserWindow()
+
+	win.on('closed', () => {
+		//Lo ponemos como nulo para que no se quede memoria en uso
+		win = null
+		//Terminamos con la ejecucion de mi aplicativo
+		app.quit()
+	})
+})
